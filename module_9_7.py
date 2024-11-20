f98d135d9b1350f544d699918d595907279e6ae1
Wartol <naruto62788@gmail.com>
2024-11-20 16:14:38 +0500

    1

--- module_9_7.py
+++ module_9_7.py
@@ -1,15 +1,12 @@
 def is_prime(func):
-    print(func())
-    if 2 % 2 == 0:
+    if func() % 2 == 0:
         print('простое')
         return func
     return func
 
-
 @is_prime
 def sum_three(*mda):
     return sum(mda)
-
 
 result = sum_three(2, 3, 6)
 print(result)
