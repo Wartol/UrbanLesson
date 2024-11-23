ffc721e6240020708d3e2fc7ff95917c52e73ab3
Wartol <naruto62788@gmail.com>
2024-11-23 19:42:36 +0500

    1

---
+++ module_10_2.py
@@ -0,0 +1,22 @@
+import threading
+import time
+class Knight(threading.Thread):
+    def __init__(self,name,power):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.power = power
+    def run(self):
+        enemy = 100
+        day = 0
+        print(f"{self.name}, на нас напали")
+        while enemy > 0:
+            day += 1
+            enemy -= self.power
+            time.sleep(1)
+            print(f"{self.name} сражается {day} осталось воинов {enemy}")
+        print(f"{self.name} одержал победу спустя {day} дней(дня)!")
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+first_knight.start()
+second_knight.start()
